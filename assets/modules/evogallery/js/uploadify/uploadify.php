3347181ee5502cb4ea60b6073f26b43a20610535
Dmi3yy <dmi3yy@gmail.com>
2013-08-08 11:08:40 +0300

    small fix

--- assets/modules/evogallery/js/uploadify/uploadify.php
+++ assets/modules/evogallery/js/uploadify/uploadify.php
@@ -26,24 +26,29 @@ THE SOFTWARE.
 die();
 /*
 if (!empty($_FILES)) {
-	$tempFile = $_FILES['Filedata']['tmp_name'];
-	$targetPath = $_SERVER['DOCUMENT_ROOT'] . $_REQUEST['folder'] . '/';
-	$targetFile =  str_replace('//','/',$targetPath) . $_FILES['Filedata']['name'];
-	
-	// $fileTypes  = str_replace('*.','',$_REQUEST['fileext']);
-	// $fileTypes  = str_replace(';','|',$fileTypes);
-	// $typesArray = split('\|',$fileTypes);
-	// $fileParts  = pathinfo($_FILES['Filedata']['name']);
-	
-	// if (in_array($fileParts['extension'],$typesArray)) {
-		// Uncomment the following line if you want to make the directory if it doesn't exist
-		// mkdir(str_replace('//','/',$targetPath), 0755, true);
-		
-		move_uploaded_file($tempFile,$targetFile);
-		echo str_replace($_SERVER['DOCUMENT_ROOT'],'',$targetFile);
-	// } else {
-	// 	echo 'Invalid file type.';
-	// }
+        $tempFile = $_FILES['Filedata']['tmp_name'];
+        $targetPath = $_SERVER['DOCUMENT_ROOT'] . $_REQUEST['folder'] . '/';
+        $targetFile =  str_replace('//','/',$targetPath) . $_FILES['Filedata']['name'];
+ 
+    // test on whitelist of extensions (don't allow evil files)
+    $info = pathinfo( $targetFile );
+    if( !in_array(strtolower($info['extension']), array('png', 'jpg', 'jpeg', 'gif')) )
+        die('wrong extension!');
+ 
+        // $fileTypes  = str_replace('*.','',$_REQUEST['fileext']);
+        // $fileTypes  = str_replace(';','|',$fileTypes);
+        // $typesArray = split('\|',$fileTypes);
+        // $fileParts  = pathinfo($_FILES['Filedata']['name']);
+ 
+        // if (in_array($fileParts['extension'],$typesArray)) {
+                // Uncomment the following line if you want to make the directory if it doesn't exist
+                // mkdir(str_replace('//','/',$targetPath), 0755, true);
+ 
+                move_uploaded_file($tempFile,$targetFile);
+                echo str_replace($_SERVER['DOCUMENT_ROOT'],'',$targetFile);
+        // } else {
+        //      echo 'Invalid file type.';
+        // }
 }
 */
 ?>
